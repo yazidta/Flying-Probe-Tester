5691d14bb10f8970a3e10d5c92875adc5a3243b9
404boboo <100701180+404boboo@users.noreply.github.com>
2025-02-02 19:46:33 +0100

    Add app_tasks.h and motorControlTask()
    - Created a command structure for motor commands to be used in motor control task
    - Added motorControlTask function which processes motor commands

---
+++ STM32/Core/Inc/app_tasks.h
@@ -0,0 +1,41 @@
+/*
+ * app_tasks.h
+ *
+ *  Created on: Feb 2, 2025
+ *      Author: Abmed Bouras
+ */
+
+#ifndef INC_APP_TASKS_H_
+#define INC_APP_TASKS_H_
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+// COMMANDS
+
+typedef enum {
+	MOTOR_CMD_MOVETO,
+	MOTOR_CMD_STOP,
+	MOTOR_CMD_START,
+	MOTOR_CMD_DIRECTION,
+} MotorCommandType;
+
+typedef struct {
+	MotorCommandType command;
+	uint8_t axisIndex;		// Index of the axis
+	uint8_t motorIndex;		// Index of the motor
+	float 	targetPositionMM;	// Target position in mm
+	uint8_t direction; 			// Motor direction
+} MotorCommand;
+
+
+
+// Function prototypes
+void motorControlTask(void *arugment);
+
+// MISC
+
+extern QueueHandle_t motorCommandQueue;
+
+
+#endif /* INC_APP_TASKS_H_ */
